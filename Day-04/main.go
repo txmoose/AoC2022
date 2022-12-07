d043f1ad97bfe0507c2a8a4112b2a189a15e86cf
Kyle Brieden <kyle@brieden.org>
2022-12-06 19:55:25 -0500

    Day 4 - second star

--- Day-04/main.go
+++ Day-04/main.go
@@ -23,35 +23,28 @@ func main() {
 		//fmt.Printf("%s || %s\n", line[0], line[1])
 		leftRange := strings.Split(line[0], "-")
 		rightRange := strings.Split(line[1], "-")
-		leftBegin, leftEnd, leftLength, err := GetLength(leftRange)
+		leftBegin, leftEnd, LeftLength, err := GetLength(leftRange)
 		if err != nil {
 			panic(err)
 		}
-		righBegin, righEnd, righLength, err := GetLength(rightRange)
+		righBegin, righEnd, RighLength, err := GetLength(rightRange)
 		if err != nil {
 			panic(err)
 		}
-
-		fmt.Printf("Checking %s || %s\n", line[0], line[1])
-		if righLength > leftLength {
-			if righBegin <= leftBegin && righEnd >= leftEnd {
-				//fmt.Println(" => Right Contains Left")
-				runningCount += 1
-				continue
-			}
-		} else if righLength < leftLength {
-			if righBegin >= leftBegin && righEnd <= leftEnd {
-				//fmt.Println(" => Left Contains Right")
-				runningCount += 1
-				continue
-			}
-		} else if righLength == leftLength {
-			if righBegin == leftBegin && righEnd == leftEnd {
-				//fmt.Println(" => Ranges are the same")
-				runningCount += 1
-				continue
-			}
+		if leftBegin <= righBegin && righBegin <= leftEnd {
+			runningCount += 1
+			continue
+		} else if leftBegin <= righEnd && righEnd <= leftEnd {
+			runningCount += 1
+			continue
+		} else if righBegin <= leftBegin && leftBegin <= righEnd {
+			runningCount += 1
+			continue
+		} else if righBegin <= leftEnd && leftEnd <= righEnd {
+			runningCount += 1
+			continue
 		}
+		fmt.Printf("%d %d\n", LeftLength, RighLength)
 	}
 	fmt.Printf("runningCount => %d", runningCount)
 }
